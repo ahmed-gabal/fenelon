e834a7d6bddd0049e8dbc6fafc232e5ac4b42d7b
ahmed-gabal <70767769+ahmed-gabal@users.noreply.github.com>
2021-10-25 13:23:27 +0200

    Update classes.js

--- classes.js
+++ classes.js
@@ -52,12 +52,4 @@ class Etudiant extends Humain
     }
 }
 
-// on crée les instances (objets)
-let marie  = new Professeur('Marie', 'France', 'F')
-let joseph = new Professeur('Joseph', 'Dupont', 'H')
-let claude = new Etudiant('Claude', 'Claudel', 'M')
-
-marie.acceuillir(joseph)
-marie.accueillir(claude)
-joseph.accueillir(marie)
-joseph.accueillir(25)
+module.exports = { Humain, Etudiant, Professeur }
